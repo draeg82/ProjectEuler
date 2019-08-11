221583fdc94512738217f9cda58b0e927ba4aa76
andymitchell <andysmitchell82@gmail.com>
2019-08-11 16:27:10 +0100

    11/08/2019 @ 16:27

---
+++ Project Euler/problem_1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Project_Euler
+{
+    class Problem_1
+    {
+        static void Main(string[] args)
+        {
+            // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+            // Find the sum of all the multiples of 3 or 5 below 1000.
+
+            var watch = Stopwatch.StartNew();
+
+            int answer = 0;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i% 5 == 0)
+                {
+                    answer += i;
+                }
+            }
+            watch.Stop();
+            Console.WriteLine("The solution is " + answer.ToString());
+            Console.WriteLine("The solution was found in " + watch.ElapsedMilliseconds.ToString() + "ms");
+        }
+    }
+}
